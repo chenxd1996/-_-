45a70046edea9b559acf28d5ca3dbc1584c2e8b4
陈旭东 <dong@chenxudongdeMacBook-Air.local>
2017-09-18 19:58:51 +0800

    ##

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,19 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
             BufferedImage img = ImageIO.read(new File("images/37.png"));
-            BufferedImage newImg = Scaler.scale(img, 192, 128);
+            System.out.print("请输入目标图片的宽度：");
+            Scanner sc = new Scanner(System.in);
+            int width = sc.nextInt();
+            System.out.print("请输入目标图片的高度：");
+            int height = sc.nextInt();
+            BufferedImage newImg = Scaler.scale(img, width, height);
             ImageIO.write(newImg, "png", new File("images/test.png"));
         } catch (IOException e) {
             e.printStackTrace();
