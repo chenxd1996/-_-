42cdb370f5bf89e8649518a2e8cccf6d62e6a114
陈旭东 <dong@chenxudongdeAir.lan>
2017-09-20 23:13:13 +0800

    ##

--- src/Scaler.java
+++ src/Scaler.java
@@ -4,7 +4,7 @@ public class Scaler {
     public static BufferedImage scale(BufferedImage img, int target_width, int target_height) {
         int width = img.getWidth();
         int height = img.getHeight();
-        BufferedImage newImg = new BufferedImage(target_width, target_height, BufferedImage.TYPE_INT_RGB);
+        BufferedImage newImg = new BufferedImage(target_width, target_height, BufferedImage.TYPE_BYTE_GRAY);
         for (int i = 0; i < target_width; i++) {
             for (int j = 0; j < target_height; j++) {
                 float x = ((float) i / target_width) * width;
@@ -13,43 +13,25 @@ public class Scaler {
                 int x2 = (int)Math.ceil(x) < width? (int)Math.ceil(x): width - 1;
                 int y1 = (int) Math.floor(y);
                 int y2 = (int) Math.ceil(y) < height? (int)Math.ceil(y): height - 1;
-                RGB rgb11 = new RGB(img.getRGB(x1, y1));
-                RGB rgb12 = new RGB(img.getRGB(x1, y2));
-                RGB rgb21 = new RGB(img.getRGB(x2, y1));
-                RGB rgb22 = new RGB(img.getRGB(x2, y2));
-                RGB rgb = new RGB(0);
+                int value11 = img.getData().getSample(x1, y1, 0);
+                int value12 = img.getData().getSample(x1, y2, 0);
+                int value21 = img.getData().getSample(x2, y1, 0);
+                int value22 = img.getData().getSample(x2, y2, 0);
+                int value;
                 if (x1 == x2 && y1 == y2) {
-                    newImg.setRGB(i, j, img.getRGB(x1, y1));
+                    newImg.getRaster().setSample(i, j, 0, value11);
                 } else if (x1 == x2) {
-                    rgb.a = (int)((y2 - y) / (y2 - y1) * rgb11.a + (y - y1) / (y2 - y1) * rgb12.a);
-                    rgb.r = (int)((y2 - y) / (y2 - y1) * rgb11.r + (y - y1) / (y2 - y1) * rgb12.r);
-                    rgb.g = (int)((y2 - y) / (y2 - y1) * rgb11.g + (y - y1) / (y2 - y1) * rgb12.g);
-                    rgb.b = (int)((y2 - y) / (y2 - y1) * rgb11.b + (y - y1) / (y2 - y1) * rgb12.b);
-                    newImg.setRGB(i, j, rgb.getRgb());
+                    value = (int)((y2 - y) / (y2 - y1) * value11 + (y - y1) / (y2 - y1) * value12);
+                    newImg.getRaster().setSample(i, j, 0, value);
                 } else if (y1 == y2) {
-                    rgb.a = (int)((x2 - x) / (x2 - x1) * rgb11.a + (x - x1) / (x2 - x1) * rgb12.a);
-                    rgb.r = (int)((x2 - x) / (x2 - x1) * rgb11.r + (x - x1) / (x2 - x1) * rgb12.r);
-                    rgb.g = (int)((x2 - x) / (x2 - x1) * rgb11.g + (x - x1) / (x2 - x1) * rgb12.g);
-                    rgb.b = (int)((x2 - x) / (x2 - x1) * rgb11.b + (x - x1) / (x2 - x1) * rgb12.b);
-                    newImg.setRGB(i, j, rgb.getRgb());
+                    value = (int)((x2 - x) / (x2 - x1) * value11 + (x - x1) / (x2 - x1) * value12);
+                    newImg.getRaster().setSample(i, j, 0, value);
                 } else {
-                    rgb.a = (int)(((float)rgb11.a /((x2 - x1) * (y2 - y1))) * (x2 - x) * (y2 - y)
-                            + ((float) rgb21.a /((x2 - x1) * (y2 - y1))) * (x - x1) * (y2 - y)
-                            + ((float) rgb12.a /((x2 - x1) * (y2 - y1))) * (x2 - x) * (y - y1)
-                            + ((float) rgb22.a /((x2 - x1) * (y2 - y1))) * (x - x1) * (y - y1));
-                    rgb.r = (int)(((float)rgb11.r /((x2 - x1) * (y2 - y1))) * (x2 - x) * (y2 - y)
-                            + ((float) rgb21.r /((x2 - x1) * (y2 - y1))) * (x - x1) * (y2 - y)
-                            + ((float) rgb12.r /((x2 - x1) * (y2 - y1))) * (x2 - x) * (y - y1)
-                            + ((float) rgb22.r /((x2 - x1) * (y2 - y1))) * (x - x1) * (y - y1));
-                    rgb.g = (int)(((float)rgb11.g /((x2 - x1) * (y2 - y1))) * (x2 - x) * (y2 - y)
-                            + ((float) rgb21.g /((x2 - x1) * (y2 - y1))) * (x - x1) * (y2 - y)
-                            + ((float) rgb12.g /((x2 - x1) * (y2 - y1))) * (x2 - x) * (y - y1)
-                            + ((float) rgb22.g /((x2 - x1) * (y2 - y1))) * (x - x1) * (y - y1));
-                    rgb.b = (int)(((float)rgb11.b /((x2 - x1) * (y2 - y1))) * (x2 - x) * (y2 - y)
-                            + ((float) rgb21.b /((x2 - x1) * (y2 - y1))) * (x - x1) * (y2 - y)
-                            + ((float) rgb12.b /((x2 - x1) * (y2 - y1))) * (x2 - x) * (y - y1)
-                            + ((float) rgb22.b /((x2 - x1) * (y2 - y1))) * (x - x1) * (y - y1));
-                    newImg.setRGB(i, j, rgb.getRgb());
+                    value = (int)(((float)value11 /((x2 - x1) * (y2 - y1))) * (x2 - x) * (y2 - y)
+                            + ((float) value21 /((x2 - x1) * (y2 - y1))) * (x - x1) * (y2 - y)
+                            + ((float) value12 /((x2 - x1) * (y2 - y1))) * (x2 - x) * (y - y1)
+                            + ((float) value22 /((x2 - x1) * (y2 - y1))) * (x - x1) * (y - y1));
+                    newImg.getRaster().setSample(i, j, 0, value);
                 }
 
             }
